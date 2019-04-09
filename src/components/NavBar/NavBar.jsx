ae24733646b92c70a6b23820696f20efe7e9d86d
lukasz.kowalski <lukasz.kowalski@MacBook-Pro.local>
2019-04-09 22:53:30 +0200

    NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,62 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import MobileMenu from './MobileMenu';
 import './NavBar.scss';
 
 class NavBar extends React.Component {
+
+  state = {
+    showMobileMenu: false,
+    mobileClassName: 'mobile-menu--hide'
+  };
+
+  handleHamburgerClick = () => {
+    this.setState({
+      showMobileMenu: true,
+      mobileClassName: 'mobile-menu--show'
+    });
+  }
+
+  handleCloseClick = () => {
+    this.setState({
+      showMobileMenu: false,
+      mobileClassName: 'mobile-menu--hide'
+    });
+  }
+
   render() {
     return (
       <nav>
-        NavBar
+        <div className='nav-container'>
+          <div className='nav-icons'>
+            <a href='https://github.com/lukasz-kowalski' target='_blank' rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} className='nav-icons__icon' />
+            </a>
+            <a href='https://www.linkedin.com/in/lukasz-kowalski-9759a6160' target='_blank' rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedinIn} className='nav-icons__icon' />
+            </a>
+          </div>
+          <div className='nav-links'>
+            <NavLink to='/'>HomePage</NavLink>
+            <NavLink to='/bio'>Bio</NavLink>
+            <NavLink to='/projects'>Projects</NavLink>
+            <NavLink to='/contact'>Contact</NavLink>
+          </div>
+          <div className='nav-hamburger'>
+            <button className='nav-hamburger__button' onClick={this.handleHamburgerClick}>
+              <FontAwesomeIcon icon={faBars} />
+            </button>
+          </div>
+        </div>
+        <MobileMenu 
+          mobileClassName={this.state.mobileClassName} 
+          handleClick={this.handleCloseClick} 
+        />
       </nav>
-    )
+    );
   }
 }
 
