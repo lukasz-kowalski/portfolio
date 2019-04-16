b5be27b6f193a43724dd62f6850912e96247e336
lukasz.kowalski <lukasz.kowalski@MacBook-Pro.local>
2019-04-16 23:13:39 +0200

    NavBar refactor

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,61 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import MobileMenu from './MobileMenu';
 
-class NavBar extends React.Component {
-  state = {
-    showMobileMenu: false,
-    mobileClassName: 'mobile-menu--hide'
-  };
+const NavBar = () => {
+  const [mobileClassName, toggleMobileClassName] = useState('mobile-menu--hide')
 
-  handleHamburgerClick = () => {
-    this.setState({
-      showMobileMenu: true,
-      mobileClassName: 'mobile-menu--show'
-    }, () => document.body.setAttribute('style', 'overflow-y: hidden'));
+  const handleHamburgerClick = () => {
+    toggleMobileClassName('mobile-menu--show')
+    document.body.setAttribute('style', 'overflow-y: hidden');
   }
 
-  handleCloseClick = () => {
-    this.setState({
-      showMobileMenu: false,
-      mobileClassName: 'mobile-menu--hide'
-    }, () => document.body.removeAttribute('style'));
+  const handleCloseClick = () => {
+    toggleMobileClassName('mobile-menu--hide')
+    document.body.removeAttribute('style');
   }
 
-  render() {
-    return (
-      <nav>
-        <div className='nav-container'>
-          <div className='nav-icons'>
-            <a href='https://github.com/lukasz-kowalski' target='_blank' rel='noopener noreferrer'>
-              <FontAwesomeIcon icon={faGithub} className='nav-icons__icon' />
-            </a>
-            <a href='https://www.linkedin.com/in/lukasz-kowalski-9759a6160' target='_blank' rel='noopener noreferrer'>
-              <FontAwesomeIcon icon={faLinkedinIn} className='nav-icons__icon' />
-            </a>
-          </div>
-          <div className='nav-links'>
-            <NavLink to='/' exact>HomePage</NavLink>
-            <NavLink to='/projects'>Projects</NavLink>
-            <NavLink to='/bio'>Bio</NavLink>
-            <NavLink to='/contact'>Contact</NavLink>
-          </div>
-          <div className='nav-hamburger'>
-            <button className='nav-hamburger__button' onClick={this.handleHamburgerClick}>
-              <FontAwesomeIcon icon={faBars} />
-            </button>
-          </div>
+  return (
+    <nav>
+      <div className='nav-container'>
+        <div className='nav-icons'>
+          <a href='https://github.com/lukasz-kowalski' target='_blank' rel='noopener noreferrer'>
+            <FontAwesomeIcon icon={faGithub} className='nav-icons__icon' />
+          </a>
+          <a href='https://www.linkedin.com/in/lukasz-kowalski-9759a6160' target='_blank' rel='noopener noreferrer'>
+            <FontAwesomeIcon icon={faLinkedinIn} className='nav-icons__icon' />
+          </a>
         </div>
-        <MobileMenu 
-          mobileClassName={this.state.mobileClassName} 
-          handleClick={this.handleCloseClick} 
-        />
-      </nav>
-    );
-  }
+        <div className='nav-links'>
+          <NavLink to='/' exact>HomePage</NavLink>
+          <NavLink to='/projects'>Projects</NavLink>
+          <NavLink to='/bio'>Bio</NavLink>
+          <NavLink to='/contact'>Contact</NavLink>
+        </div>
+        <div className='nav-hamburger'>
+          <button className='nav-hamburger__button' onClick={handleHamburgerClick}>
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+        </div>
+      </div>
+      <MobileMenu 
+        mobileClassName={mobileClassName} 
+        handleClick={handleCloseClick} 
+      />
+    </nav>
+  );
 }
 
 export default withRouter(NavBar);
